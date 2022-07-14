aa76958b23262ce6c3e5d55a81c7cc82f6b1beef
Lidor Bekel <74781212+lidorbekel@users.noreply.github.com>
2022-07-15 01:08:16 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Lidor, the project is ready, have fun!" />
+      <HelloWorld msg="Lidor, the project is ready, Congrats!!!!" />
     </div>
   </header>
 
